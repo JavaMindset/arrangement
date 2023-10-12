17133be8144f2aade83e6537f7eff8bd122b75b0
Aziz6 <ikromov.aa22@gmail.com>
2023-10-12 17:05:45 +0500

    Initial commit

---
+++ src/main/java/com/example/arrangement/ArrangementApplication.java
@@ -0,0 +1,13 @@
+package com.example.arrangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrangementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArrangementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/arrangement/ArrangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.arrangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArrangementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
